fe9fe9951be259c97ef546ec2f4b48cac055a642
Zaky Irsyad Rais <zkyxentertain@gmail.com>
2024-02-22 15:29:35 +0700

    update Links

--- src/organism/Links.jsx
+++ src/organism/Links.jsx
@@ -3,21 +3,27 @@ import ButtonLink from '@/components/ButtonLink'
 import Link from 'next/link'
 import React from 'react'
 
+const linkCV = 'https://utaboiwoepvwdhnczsgd.supabase.co/storage/v1/object/sign/project/Curriculum%20Vitae%20-%20Zaky%20Irsyad%20Rais.pdf?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJwcm9qZWN0L0N1cnJpY3VsdW0gVml0YWUgLSBaYWt5IElyc3lhZCBSYWlzLnBkZiIsImlhdCI6MTcwNzE2MTkxMiwiZXhwIjoxNzM4Njk3OTEyfQ.eLLdmJ3xaB6BC1GavfNz-5hqR5tU9AvXDjDjDmMX1yc&t=2024-02-05T19%3A38%3A32.472Z'
 export default function Links() {
     return (
         <div className='flex flex-col gap-5 mx-5'>
-            <ButtonLink>
-                Visit My Website
-            </ButtonLink>
-            <ButtonCvLink>
-                Curriculum Vitae
-            </ButtonCvLink>
+            <Link href={'/'}>
+                <ButtonLink>
+                    Visit My Website
+                </ButtonLink>
+            </Link>
+
+            <Link href={linkCV}>
+                <ButtonLink>
+                    Curriculum Vitae
+                </ButtonLink>
+            </Link>
             <Link href='www.linkedin.com/in/zakyirsyad' target='_blank'>
                 <ButtonLink>
                     Linked-in
                 </ButtonLink>
             </Link>
 
-        </div>
+        </div >
     )
 }
